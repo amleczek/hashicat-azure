a225d3b6849158663298e9d6417dd655767e7bb2
agamleczek <agamleczek@gmail.com>
2022-04-11 20:30:29 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -110,8 +110,9 @@ resource "azurerm_virtual_machine" "catapp" {
 
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
-  Department = "devops"
   Billable = "true" 
+  Department = "devops"
+  Environment = "prod"
   
   storage_image_reference {
     publisher = var.image_publisher
