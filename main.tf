aac33882cdacff80bd9018c587e94781c6830d57
agamleczek <agamleczek@gmail.com>
2022-04-12 13:48:03 -0400

    Added remote backend

--- main.tf
+++ main.tf
@@ -7,10 +7,14 @@ terraform {
   }
 }
 
+
 provider "azurerm" {
+   
+    tenant_id = "87c90862-1c43-40ff-b612-d9797dd26ff8"
   features {}
 
 }
+
 
 resource "azurerm_resource_group" "myresourcegroup" {
   name     = "${var.prefix}-workshop"
