973480dd95823cc5edae8207348952b5840a4e22
agamleczek <agamleczek@gmail.com>
2022-04-11 20:17:11 -0400

    Added remote backend

--- remote_backend.tf
+++ remote_backend.tf
@@ -2,6 +2,8 @@ terraform {
   backend "remote" {
     hostname = "app.terraform.io"
     organization = "Azure-terraform-Lab1"
+    token = "bpSiM8H4Ko2a6g.atlasv1.ImV1UaZQUriyz5LwK5EPq2lHhnqz5Cc7SUuMHhSWrCVhi3hAoyWWiBCmbMlpgJsGoRE"
+  
     workspaces {
       name = "hashicat-azure"
     }
